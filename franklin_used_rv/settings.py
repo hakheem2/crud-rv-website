fab80fa45fb62592fe6f673af3fc6bc191e1f7e3
Anyeng Mondeson <anyengmondesonmbaubeh@gmail.com>
2025-08-19 19:49:35 +0100

    etting storage for media upload

--- franklin_used_rv/settings.py
+++ franklin_used_rv/settings.py
@@ -80,20 +80,19 @@ WSGI_APPLICATION = 'franklin_used_rv.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+if os.environ.get("DJANGO_ENV") == "production":
+    DATABASES = {
+        "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
