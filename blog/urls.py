ba0df1743e22fa541ad96a69a6ec214800ebc895
Anyeng Mondeson <anyengmondesonmbaubeh@gmail.com>
2025-08-19 21:11:51 +0100

    Remove all files from git tracking

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('trip-inspirations/', views.blog, name='blog'),
-    path('trip-inspirations/<int:slug>/', views.blog_post, name='blog_post'),
-]
--- blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def blog(request):
-    return render(request, 'blog.html')
-
-def blog_post(request):
-    return render(request, 'blog-post.html')
