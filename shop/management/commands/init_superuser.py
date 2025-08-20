064335da66e3bb2b504e9eb3bfbdacded5365e1e
Anyeng Mondeson <anyengmondesonmbaubeh@gmail.com>
2025-08-20 06:01:06 +0100

    chages for start log

--- shop/management/commands/init_superuser.py
+++ shop/management/commands/init_superuser.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
         username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
-        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "franklinusedrv@gmail.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "franklin123.RV")
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
